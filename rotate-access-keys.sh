90c25e1efa9195b9b8cc0d2024743283c55bd8db
morgan stake <morgan.stake@cgi.com>
2018-04-03 08:48:58 -0600

    initial commit

--- rotate-access-keys.sh
+++ rotate-access-keys.sh
@@ -47,10 +47,13 @@ function getArrayIndex()
 function displayUsage() {
     echo "usage: $0.sh [options...] "
     echo "options:"
-    echo " -i  The desired Cloud Platform (IAAS)" `printf '%s\n' "${IAAS_OPTIONS[@]}"`
-    echo " -1  Preview the users needing rotation"
+    echo " -a  AWS Access Credentials file ( defaults to ~/.aws/credentials ) "
+    echo " -i  The desired Cloud Platform (IAAS): ( ${IAAS_OPTIONS[@]:1})"
     echo " -d  The Number of days since the last rotation, indicating expiry"
     echo "     (-d 15 = 15 days and older will be rotated)"
+    echo " -h  This help text"
+    echo " -s  S3 Bucket filename to test access for the new keys."
+    echo " -1  Preview the users needing rotation"
 
 }
 
@@ -60,9 +63,18 @@ function rotateAzure() {
 
 function rotateAws() {
 
-  echo Rotating AWS User Id Access Keys On or before $1
-  AllActiveUsers=$(for i in `aws iam list-users |jq -r '.Users[].UserName'` ; do aws iam list-access-keys  --user-name $i| jq -j -r '.AccessKeyMetadata[]|select(.Status=="Active")|.UserName,"=",.CreateDate, "\n"';done)
-#  AllActiveUsers="anault=2018-03-17T22:07:59Z cgi-concourse-user=2018-03-30T22:30:18Z Darwesh-remote=2018-03-30T22:08:15Z darwesh.singh@cgi.com=2018-03-30T22:08:31Z dbrown=2018-03-30T22:08:46Z ecs-concourse-ci=2018-03-30T21:52:09Z ganderson=2018-03-30T22:09:04Z jlutz=2018-03-30T22:09:20Z jslCfUser=2018-03-30T22:09:40Z kkellner=2018-03-22T00:02:51Z mcarlson=2018-03-30T20:56:56Z mgw=2018-03-30T20:33:27Z mlove=2018-03-30T22:09:59Z mminges=2018-03-30T22:10:15Z morgan-concourse=2018-03-30T20:03:28Z morgan.stake@cgi.com=2018-03-30T20:24:14Z pcf-stack-PcfIamUser-1CW4P398VBY72=2018-03-30T22:10:35Z pcf17admin=2018-03-30T22:10:50Z rdrew=2018-03-30T22:11:08Z rlargent=2018-03-30T22:11:23Z rsambandan=2018-03-30T22:11:39Z ryslas=2018-03-30T22:11:58Z shutto=2018-03-30T22:12:14Z sraja=2018-03-30T22:12:29Z sravan.neppalli@cgi.com=2018-03-30T22:12:49Z swall=2018-03-30T22:13:06Z tanderson=2018-03-30T20:50:22Z tariq=2018-03-30T16:59:40Z tsiddiqui=2018-03-30T22:13:23Z tyslas=2018-03-30T22:13:39Z"
+  echo "Rotating AWS User Id Access Keys On or before $1"
+  if [ -z ${S3_TEST_FILE} ]
+  then
+    echo "We will need an S3 Bucket filename to test access for the new keys."
+    echo "URL format for the Bucket filename <s3://bucketName/filename>"
+    read -p "Bucket filename: " S3_TEST_FILE
+  else
+    echo "S3 Bucket filename to test access for the new keys: "${S3_TEST_FILE}
+  fi
+
+#  AllActiveUsers=$(for i in `aws iam list-users |jq -r '.Users[].UserName'` ; do aws iam list-access-keys  --user-name $i| jq -j -r '.AccessKeyMetadata[]|select(.Status=="Active")|.UserName,"=",.CreateDate, "\n"';done)
+  AllActiveUsers="anault=2018-03-17T22:07:59Z cgi-concourse-user=2018-03-30T22:30:18Z Darwesh-remote=2018-03-30T22:08:15Z darwesh.singh@cgi.com=2018-03-30T22:08:31Z dbrown=2018-03-30T22:08:46Z ecs-concourse-ci=2018-03-30T21:52:09Z ganderson=2018-03-30T22:09:04Z jlutz=2018-03-30T22:09:20Z jslCfUser=2018-03-30T22:09:40Z kkellner=2018-03-22T00:02:51Z mcarlson=2018-03-30T20:56:56Z mgw=2018-03-30T20:33:27Z mlove=2018-03-30T22:09:59Z mminges=2018-03-30T22:10:15Z morgan-concourse=2018-03-30T20:03:28Z morgan.stake@cgi.com=2018-03-30T20:24:14Z pcf-stack-PcfIamUser-1CW4P398VBY72=2018-03-30T22:10:35Z pcf17admin=2018-03-30T22:10:50Z rdrew=2018-03-30T22:11:08Z rlargent=2018-03-30T22:11:23Z rsambandan=2018-03-30T22:11:39Z ryslas=2018-03-30T22:11:58Z shutto=2018-03-30T22:12:14Z sraja=2018-03-30T22:12:29Z sravan.neppalli@cgi.com=2018-03-30T22:12:49Z swall=2018-03-30T22:13:06Z tanderson=2018-03-30T20:50:22Z tariq=2018-03-30T16:59:40Z tsiddiqui=2018-03-30T22:13:23Z tyslas=2018-03-30T22:13:39Z"
   needingRotation=0
   for i in $AllActiveUsers
   do
@@ -97,10 +109,10 @@ fi
 IAAS=-1
 VALID_OPTION=""
 ACCESS_KEY_FILE=${ACCESS_KEY_FILE:-"~/.aws/credentials"}
-S3_TEST_FILE="s3://morgan-test2/junk.test"
+S3_TEST_FILE=""
 PREVIEW_COMMAND=""
 
-while getopts ":i:d:a:1" opt; do
+while getopts ":s:i:d:a:1h" opt; do
   case ${opt} in
     1)
       PREVIEW_COMMAND="echo "
@@ -117,9 +129,15 @@ while getopts ":i:d:a:1" opt; do
       days=$OPTARG
       expiration_date=$(date -v -${days}d +%Y-%m-%d)
       ;;
-    \?)
+    h)
       displayUsage
       exit 1
+      ;;
+    s)
+      S3_TEST_FILE=$OPTARG
+      ;;
+    a)
+      ACCESS_KEY_FILE=$OPTARG
       ;;
   esac
 done
@@ -136,11 +154,11 @@ then
   IAAS=$MENU_SELECTION_POSITION
 else
   MENU_SELECTION_POSITION=$IAAS
-  MENU_SELECTION=$IAAS_OPTIONS[$IAAS]
+  MENU_SELECTION=${IAAS_OPTIONS[${IAAS} - 1]}
 fi
 
 echo "IAAS: $MENU_SELECTION selected"
-echo ${IAAS}
+
 case ${IAAS} in
     1)
       echo "Aborting execution..."
